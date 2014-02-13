b63250b1494b1a133eff502c1c14917f9ba385a5
Jiihaa <ext-jani.1.nevalainen@nokia.com>
2014-02-13 16:17:00 +0200

    Added some default values for constructors to show better the filters in use, and updated the metadata

--- ImagingSDKSamples/Properties/AssemblyInfo.cs
+++ ImagingSDKSamples/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ImagingSDKSamples")]
+[assembly: AssemblyTitle("Simple Filter Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImagingSDKSamples")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Simple Filter Sample")]
+[assembly: AssemblyCopyright("Copyright © Nokia 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
